80b05b9d3a479c5c54c721a212980f28e5362d6c
cadif1 <cadif1@hotmail.com>
2020-01-29 14:32:39 -0400

    guardando las practicas

---
+++ Clase1/js/clase1.js
@@ -0,0 +1,14 @@
+console.log("Soy la mejor programadora que existe");
+var x;
+console.log("x:" + x);
+var y= 1;
+console.log("y:" + y);
+x+=y;
+console.log("x:" + x);
+x=100;
+console.log("x:"+ x);
+x="Mariana"
+console.log("x:" + x);
+x=true;
+x++;
+console.log("x:" + x);
---
+++ Clase1/js/clase1_1.js
@@ -0,0 +1,13 @@
+console.log("Inicio del script de clase1_1.js");
+//Crear un numero aleatorio entre 0 y 20
+
+var n = Math.random()*20;
+console.log("n=" + n);
+var val_maximo;
+
+
+
+
+console.log("Inicio del script de clase1_1.js");
+
+
